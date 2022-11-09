4296f0a9e22920b7cb7d0e754ded335a8a10902f
hnishino <hnishino@lc-ap1.jim.kochi-tech.ac.jp>
2022-11-09 13:05:45 +0900

    Initial Commit for Ex01

---
+++ src/kut/compiler/parser/Parser.java
@@ -0,0 +1,90 @@
+package kut.compiler.parser;
+
+import java.io.IOException;
+
+import kut.compiler.exception.CompileErrorException;
+import kut.compiler.exception.SyntaxErrorException;
+import kut.compiler.lexer.Lexer;
+import kut.compiler.lexer.Token;
+import kut.compiler.lexer.TokenClass;
+import kut.compiler.parser.ast.AstNode;
+import kut.compiler.parser.ast.AstIntLiteral;
+import kut.compiler.parser.ast.AstProgram;
+
+/**
+ * @author hnishino
+ *
+ */
+public class Parser 
+{
+	protected Lexer 	lexer;
+	protected Token		currentToken;
+	
+	/**
+	 * @param lexer
+	 */
+	public Parser(Lexer lexer) {
+		this.lexer = lexer;
+	}
+	
+	
+	
+	/**
+	 * @return
+	 */
+	protected Token getCurrentToken()
+	{
+		return this.currentToken;
+	}
+	
+	/**
+	 * 
+	 */
+	/**
+	 * 
+	 */
+	protected void consumeCurrentToken() throws IOException
+	{
+		this.currentToken = lexer.getNextToken();
+		return;
+	}
+	
+	/**
+	 * @return
+	 */
+	public AstNode parse() throws IOException, CompileErrorException
+	{
+		//read the first token.
+		consumeCurrentToken();
+		return program();
+	}
+	 
+	/**
+	 * @return
+	 */
+	protected AstNode program() throws IOException, CompileErrorException
+	{
+		AstNode node = integer();
+		return new AstProgram(node);
+	}
+	
+	/**
+	 * @return
+	 * @throws IOException
+	 * @throws CompileErrorException
+	 */
+	protected AstNode integer() throws IOException, CompileErrorException
+	{
+		Token t = this.getCurrentToken();
+		if (t.getC() != TokenClass.IntLiteral) {
+			throw new SyntaxErrorException("expected an integer literal, but found: " + t.getL());
+		}
+		
+		AstNode node = new AstIntLiteral(t);
+		this.consumeCurrentToken();
+		
+		return node;
+	}
+	
+	
+}
---
+++ src/kut/compiler/parser/ast/AstNode.java
@@ -0,0 +1,18 @@
+package kut.compiler.parser.ast;
+
+
+import kut.compiler.compiler.CodeGenerator;
+
+/**
+ * @author hnishino
+ *
+ */
+public abstract class AstNode {
+	
+	
+		/**
+		 * @return the generated code string
+		 */
+		public abstract void cgen(CodeGenerator gen);
+		
+}
